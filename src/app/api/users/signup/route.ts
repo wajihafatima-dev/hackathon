7aacf8728de589e7d29b291edca50774ffa164ba
wajihafatima-dev <wajihafatima689@gmail.com>
2024-10-23 16:12:58 +0500

    .

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,24 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB } from '@/dbconfig/dbConfig';
 import usermodel from '@/models/usermodel';
 import bcrypt from 'bcryptjs';
 
-export async function POST(request: Request) {
-  const startTime = Date.now();
 
+connectDB();
+export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
     const { firstName, lastName, email, password } = reqBody;
-
-    console.log('Checking for existing user...');
-    const existingUser = await usermodel.findOne({ email }).exec(); // Ensure the query is efficient
+    const existingUser = await usermodel.findOne({ email });
     if (existingUser) {
       return NextResponse.json(
         { error: 'This email already exists' },
         { status: 400 }
       );
     }
-
-    console.log('Hashing password...');
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new usermodel({
@@ -28,11 +25,7 @@ export async function POST(request: Request) {
       password: hashedPassword,
     });
 
-    console.log('Saving new user...');
     await newUser.save();
-
-    console.log(`Request processed in ${Date.now() - startTime}ms`);
-
     return NextResponse.json({
       message: 'User created successfully',
       user: {
@@ -43,7 +36,6 @@ export async function POST(request: Request) {
       },
     });
   } catch (error: any) {
-    console.error('Error:', error);
     return NextResponse.json(
       { error: error.message },
       { status: 500 }
