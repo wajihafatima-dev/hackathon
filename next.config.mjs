39dd7e62bf95b53b56b979cdfff06267043d5c3c
wajihafatima-dev <wajihafatima689@gmail.com>
2024-10-23 18:44:38 +0500

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes under /api/*
+          source: '/api/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*', // Allow any origin
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET, POST, PUT, DELETE, OPTIONS',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
